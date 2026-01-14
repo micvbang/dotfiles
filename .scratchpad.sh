52e94f7b7f4b469ea796ef04f1963ae123ba4a24
Michael Bang <mic@vbang.dk>
2026-01-14 09:41:55 +0100

    scratchpad: update for ubuntu 24.04

--- .scratchpad.sh
+++ .scratchpad.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-WIDTH_PCT=90
-HEIGHT_PCT=90
+WIDTH_PCT=75
+HEIGHT_PCT=75
 
 # get width of screen and height of screen
 SCREEN_WIDTH=$(xwininfo -root | awk '$1=="Width:" {print $2}')
 SCREEN_HEIGHT=$(xwininfo -root | awk '$1=="Height:" {print $2}')
 
-# new width and height
-W=$(( ($SCREEN_WIDTH / 100 * $WIDTH_PCT) / 11 ))
-H=$(( ($SCREEN_HEIGHT / 100 * $HEIGHT_PCT)  / 22))
-X=$(( $SCREEN_WIDTH / 100 * (100 - $WIDTH_PCT) ))
-Y=$(( $SCREEN_HEIGHT / 100 * (100 - $HEIGHT_PCT) ))
+# new width and height (in characters for terminal)
+W=$(( ($SCREEN_WIDTH / 100 * $WIDTH_PCT) / 10 ))
+H=$(( ($SCREEN_HEIGHT / 100 * $HEIGHT_PCT) / 20 ))
 
-gnome-terminal --geometry $(printf "%sx%s+%s+%s" "$W" "$H" "$X" "$Y")  -- sh -c "tmux attach -dt scratchpad || tmux new -s scratchpad"
-# wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz && wmctrl -r :ACTIVE: -e 0,$X,$Y,$W,$H
+# Calculate pixel dimensions and center position
+PIXEL_W=$(( $SCREEN_WIDTH / 100 * $WIDTH_PCT ))
+PIXEL_H=$(( $SCREEN_HEIGHT / 100 * $HEIGHT_PCT ))
+X=$(( ($SCREEN_WIDTH - $PIXEL_W) / 2 ))
+Y=$(( ($SCREEN_HEIGHT - $PIXEL_H) / 2 ))
+
+ghostty --window-width="$W" --window-height="$H" -e sh -c "tmux attach -dt scratchpad || tmux new -s scratchpad" &
